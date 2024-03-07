e0c63462dea3ff821acb6269eb75aee31f628e9f
Mitsurugi <ha.bok.hada@gmail.com>
2024-03-06 22:04:59 -0600

    Add files via upload

---
+++ lang/en/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Se ha restablecido la contraseña correctamente.',
+    'sent' => 'Se ha enviado un email para restablecer la contraseña.',
+    'throttled' => 'Espere antes de volver a intentarlo.',
+    'token' => 'Este token es inválido.',
+    'user' => "No hemos encontrado al usuario indicado por el correo.",
+
+];
